4013fe3ea615e35d3591a301401f5590c7c93995
tanishq0721 <132347016+tanishq0721@users.noreply.github.com>
2025-03-31 18:05:26 +0000

    Javascript

---
+++ JS/5.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js as new version 
+// alert( 3 + 3 ) // Give error as we r using nodejs not browser 
+
+console.log( 3 + 3 ) 
+
+let name = "Tanishq" // String datatype
+let age = 18 // Number datatype
+
+let isLoggedin = false 
+let temp = null   // Like we didn't get any temp so used null
+
+/*  Datatypes
+    number -> 2 to power 53
+    bigint -> for large numbers
+    string -> assembly of characters
+    boolean -> true or false
+    null -> Standalone Value
+    Undefined
+    Symbol -> Used for uniqueness
+    Object
+*/
+
+console.log(typeof(age));
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)  // Object
